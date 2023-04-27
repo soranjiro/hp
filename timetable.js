53b0adcd341afc0958016024277083545a570d9b
soranjiro <122503868+soranjiro@users.noreply.github.com>
2023-04-27 22:22:53 +0900

    Add files via upload

---
+++ timetable.js
@@ -0,0 +1,118 @@
+const id11 = document.getElementById('11');
+id11.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170535_1/cl/outline';
+}
+
+const id13 = document.getElementById('13');
+id13.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170564_1/cl/outline';
+}
+
+const id21 = document.getElementById('21');
+id21.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170605_1/cl/outline';
+}
+
+const id23 = document.getElementById('23');
+id23.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170555_1/cl/outline';
+}
+
+const id24 = document.getElementById('24');
+id24.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170509_1/cl/outline';
+}
+
+const id32 = document.getElementById('32');
+id32.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170612_1/cl/outline';
+}
+
+const id33 = document.getElementById('33');
+id33.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170566_1/cl/outline';
+}
+
+const id34 = document.getElementById('34');
+id34.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170613_1/cl/outline';
+}
+
+const id35 = document.getElementById('35');
+id35.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170629_1/cl/outline';
+}
+
+const id41 = document.getElementById('41');
+id41.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170603_1/cl/outline';
+}
+
+const id42 = document.getElementById('42');
+id42.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170567_1/cl/outline';
+}
+
+const id43 = document.getElementById('43');
+id43.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170629_1/cl/outline';
+}
+
+const id44 = document.getElementById('44');
+id44.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170629_1/cl/outline';
+}
+
+
+const id51 = document.getElementById('51');
+id51.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170560_1/cl/outline';
+}
+
+const id52 = document.getElementById('52');
+id52.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170557_1/cl/outline';
+}
+
+const id53 = document.getElementById('53');
+id53.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170606_1/cl/outline';
+}
+
+const id55 = document.getElementById('55');
+id55.onclick = function(){
+    window.location.href = 'https://www.cle.osaka-u.ac.jp/ultra/courses/_170606_1/cl/outline';
+}
+
+/*
+const rows = document.querySelectorAll('tr');
+for (let i = 1; i < rows.length; i++) {
+    const link = document.createElement('a');
+    link.href = 'https://www.cle.osaka-u.ac.jp/';
+    rows[i].children[1].appendChild(link);
+    link.outerHTML = '<a href="https://www.cle.osaka-u.ac.jp/"></a>';
+
+    const link2 = document.createElement('a');
+    link2.href = 'https://www.cle.osaka-u.ac.jp/';
+    rows[i].children[2].appendChild(link2);
+    link2.outerHTML = '<a href="https://www.cle.osaka-u.ac.jp/">数学</a>';
+
+    const link3 = document.createElement('a');
+    link3.href = 'https://www.cle.osaka-u.ac.jp/';
+    rows[i].children[3].appendChild(link3);
+    link3.outerHTML = '<a href="https://www.cle.osaka-u.ac.jp/">理科</a>';
+
+    const link4 = document.createElement('a');
+    link4.href = 'https://www.cle.osaka-u.ac.jp/';
+    rows[i].children[4].appendChild(link4);
+    link4.outerHTML = '<a href="https://www.cle.osaka-u.ac.jp/">社会</a>';
+
+    const link5 = document.createElement('a');
+    link5.href = 'https://www.cle.osaka-u.ac.jp/';
+    rows[i].children[5].appendChild(link5);
+    link5.outerHTML = '<a href="https://www.cle.osaka-u.ac.jp/">国語</a>';
+}*/
+
+
+
+
